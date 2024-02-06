6db30cc753409e8ae9676b9fceac62e101fa9ba1
Alexsander Leal <alexsander.leal86@gmail.com>
2024-02-06 17:59:24 +0000

    inicio do curso

---
+++ app.js
@@ -0,0 +1,7 @@
+alert('Boas vindas ao jogo do númeor secreto!');
+let numeroSecreto = 29
+let chute = prompt('Escolha um número entre 1 e 30');
+
+if (chute == numeroSecreto) {
+    console.log('Você descobriu o número secreto')
+}
